8aa03f022251c30d5152d3ed2e916c32711c797e
astrasz <a.straszecka@gmail.com>
2022-12-17 22:13:55 +0100

    Initial commit:

---
+++ backend/src/Entity/Movie.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MovieRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MovieRepository::class)]
+class Movie
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private int $id;
+
+    #[ORM\Column(length: 255)]
+    private string $name;
+
+    #[ORM\Column(length: 255)]
+    private string $destination;
+
+    #[ORM\Column(length: 255)]
+    private string $season;
+
+    #[ORM\Column(length: 255)]
+    private string $linkToFile;
+
+    #[ORM\ManyToMany(targetEntity: Category::class, mappedBy: 'movies')]
+    private Collection $categories;
+
+    public function __construct()
+    {
+        $this->categories = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDestination(): string
+    {
+        return $this->destination;
+    }
+
+    public function setDestination(string $destination): self
+    {
+        $this->destination = $destination;
+
+        return $this;
+    }
+
+    public function getSeason(): string
+    {
+        return $this->season;
+    }
+
+    public function setSeason(string $season): self
+    {
+        $this->season = $season;
+
+        return $this;
+    }
+
+    public function getLinkToFile(): string
+    {
+        return $this->linkToFile;
+    }
+
+    public function setLinkToFile(string $linkToFile): self
+    {
+        $this->linkToFile = $linkToFile;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Category>
+     */
+    public function getCategories(): Collection
+    {
+        return $this->categories;
+    }
+
+    public function addCategory(Category $category): self
+    {
+        if (!$this->categories->contains($category)) {
+            $this->categories->add($category);
+            $category->addMovie($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCategory(Category $category): self
+    {
+        if ($this->categories->removeElement($category)) {
+            $category->removeMovie($this);
+        }
+
+        return $this;
+    }
+}
